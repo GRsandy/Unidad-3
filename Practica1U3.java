d9fd8726e9156f4be46411897395fb604f2c7f1b
GRsandy <143724905+GRsandy@users.noreply.github.com>
2023-09-26 18:14:01 -0600

    Practica1U3.java

--- Practica1U3.java
+++ Practica1U3.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package practica1.u3;
-
+package u3.practica1;
+import java.util.Scanner;
 /**
  *
- * @author hp
+ * @author sandy
  */
-public class Practica1U3 {
+public class U3Practica1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero");
+        int num=scanner.nextInt();  
+        if (num%2==0){
+            System.out.println("El2 numero es par");
+        }
+        else{
+            System.out.println("El numero no es par");
+        }
+    } 
 }
+    
